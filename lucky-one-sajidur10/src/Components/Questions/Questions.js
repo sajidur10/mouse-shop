798468ed7e689f76820c012559e3db217f891c42
sajidur10 <msrrahatbd1@gmail.com>
2022-03-26 23:42:45 +0600

    ans

--- lucky-one-sajidur10/src/Components/Questions/Questions.js
+++ lucky-one-sajidur10/src/Components/Questions/Questions.js
@@ -21,7 +21,7 @@ const Questions = () => {
                             data-bs-parent="#accordionFlushExample">
                             <div class="accordion-body">
                                 <p>
-                                    React is a javascript library.
+                                    React is a javascript library. The rest of the application is downloaded in the browser and the rendering process begins.React is a declarative, efficient, and flexible JavaScript library for building user interfaces. At its very core, React just maintains a tree for you and it will do efficient diff computations on the nodes. React.js is generally thought of as the view layer in an application.It is maintained by Facebook.
                                 </p>
                             </div>
                         </div>
@@ -37,7 +37,7 @@ const Questions = () => {
                             data-bs-parent="#accordionFlushExample">
                             <div class="accordion-body">
                                 <p>
-
+                                    Hooks are a new addition in React 16.8. The useState() is a Hook that allows you to have state variables in functional components. The useState hook is a special function that takes the initial state as an argument and returns an array of two entries. useState is a hook that lets you add state to a functional component. The first element is the initial state and the second one is a function that is used for updating the state.
                                 </p>
                             </div>
                         </div>
